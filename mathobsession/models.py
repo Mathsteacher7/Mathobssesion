ca60665c74abb84bc8825f350a130bd51a714824
Daniel H Cohen <hofcohen@gmail.com>
2019-09-05 10:39:49 +0100

    basic sketches are in, and related to the exercises

--- mathobsession/models.py
+++ mathobsession/models.py
@@ -10,11 +10,21 @@ class Subject(models.Model):
     def __str__(self):
         return self.name
 
+class Sketch(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Exercise(models.Model):
     content = models.CharField(max_length=200)
     level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(3)])
     user = models.ForeignKey(User, related_name='exercises', on_delete=models.CASCADE)
     subjects = models.ManyToManyField(Subject, related_name='exercises', blank=True)
+    sketch = models.ForeignKey(Sketch, related_name='exericises', blank=True, null=True, on_delete=models.CASCADE)
+
 
 
     def __str__(self):
